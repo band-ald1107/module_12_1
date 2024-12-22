88dd8c538220132c42407506b1724dadd47ccd6c
band-ald1107 <150985329+band-ald1107@users.noreply.github.com>
2024-12-22 20:22:42 +0800

    Add files via upload

---
+++ module_12_1.py
@@ -0,0 +1,45 @@
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+
+import unittest
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner = Runner(name="Test Runner")
+        for _ in range(10):
+            runner.walk()
+
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner(name="Test Runner")
+        for _ in range(10):
+            runner.run()
+
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner(name="Runner 1")
+        runner2 = Runner(name="Runner 2")
+
+        for _ in range(10):
+            runner1.run()
+            runner2.walk()
+
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
